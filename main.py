df1bf53d41c7cdf70e22249016fa7bf463765881
Mehrubon <128034429+mehrubon2008@users.noreply.github.com>
2023-04-07 10:22:38 +0000

    main.py

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-''' @avtor --NABIEVMEHRUBON--'''
-import turtle
-turtle.bgcolor('blueviolet')
-from turtle import Turtle, mainloop
-from time import perf_counter as clock
+'''nabiev mehrubon '''
 
-def tree(plist, l, a, f):
-    if l > 3:
-        lst = []
-        for p in plist:
-            p.forward(l)
-            q = p.clone()
-            p.left(a)
-            q.right(a)
-            lst.append(p)
-            lst.append(q)
-        for x in tree(lst, l*f, a, f):
-            yield None
-def maketree():
-    p = Turtle()
-    p.pensize(10)
-    p.setundobuffer(None)
-    p.hideturtle()
-    p.speed(0)
-    p.getscreen().tracer(30,0)
-    p.left(90)
-    p.penup()
-    p.forward(-210)
-    p.pendown()
-    t = tree([p], 200, 65, 0.6375)
-    for x in t:
-        pass
-def main():
-    a=clock()
-    maketree()
-    b=clock()
-    return "done: %.2f sec." % (b-a)
-if __name__ == "__main__":
-    msg = main()
-    print(msg)
-    mainloop()
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+# Устанавливаем уровень логов
+logging.basicConfig(level=logging.INFO)
+# Создаем бота и диспетчер
+bot = Bot(token='5841295660:AAGSAxxH62qF1-aG-vF5-yc4Ccb9qmmg1Qg')
+dp = Dispatcher(bot)
+# Обрабатываем команду /start
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer('Привет! я мехрубон.')
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+###############################
